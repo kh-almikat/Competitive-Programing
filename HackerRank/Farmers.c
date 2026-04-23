08eb6303ff2211f3dc698e7907f13688629a1986
Khondokar Almikat <khondoker.almikat@gmail.com>
2026-04-23 23:53:54 +0600

    Add files via upload

---
+++ HackerRank/Farmers.c
@@ -0,0 +1,24 @@
+/*problem link: https://www.hackerrank.com/contests/mid-term-a-introduction-to-c-programming-a-batch-09/challenges/farmers-1 */
+
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+
+    while(n--)
+    {
+        int M1, M2, D;
+        scanf("%d %d %d", &M1, &M2, &D);
+
+        int total_work = M1 * D;
+        int new_days = total_work / (M1 + M2);
+
+        int result= D - new_days;
+
+        printf("%d\n", result);
+    }
+    
+    return 0;
+}

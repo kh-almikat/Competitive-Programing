191006d4e753bd037fe0768e76d17d79937f913b
Khondokar Almikat <khondoker.almikat@gmail.com>
2026-05-17 17:00:37 +0600

    Add files via upload

---
+++ CodeForces/K_Max_Number.cpp
@@ -0,0 +1,19 @@
+/*problem link: https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/K */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.rbegin(), v.rend());
+
+    cout << v[0];
+
+}

b6d27301733028090e5346c5d444b5d73383e14f
Khondokar Almikat <khondoker.almikat@gmail.com>
2026-05-22 20:55:59 +0600

    Add files via upload

---
+++ CodeForces/A_Puzzles.cpp
@@ -0,0 +1,32 @@
+/*problem link: https://codeforces.com/problemset/problem/337/A */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a(m);
+
+    for (int i = 0; i < m; i++) 
+    {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    int ans = INT_MAX;
+
+    for(int i = 0; i <= m - n; i++)
+    {
+        int diff = a[i + n - 1] - a[i];
+
+        ans = min(ans, diff);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}

cf3f1f018a6d561a44e965418837a7fc25526b4f
Khondokar Almikat <khondoker.almikat@gmail.com>
2026-09-01 20:43:27 +0600

    Add files via upload

---
+++ CodeForces/A_Make_it_White.cpp
@@ -0,0 +1,33 @@
+/*problem link: https://codeforces.com/problemset/problem/1927/A */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        int first = -1, last = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if(s[i] == 'B')
+            {
+                if(first == -1)
+                    first = i;
+
+                last = i;
+            }
+        }
+
+        cout << last - first + 1 << endl;
+    }
+}

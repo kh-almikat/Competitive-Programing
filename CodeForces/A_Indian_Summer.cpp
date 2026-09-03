c8e529c02a568f36fc01efaea041a83bdc911f12
Khondokar Almikat <khondoker.almikat@gmail.com>
2026-09-03 23:15:28 +0600

    Add files via upload

---
+++ CodeForces/A_Indian_Summer.cpp
@@ -0,0 +1,19 @@
+/*problem link: https://codeforces.com/problemset/problem/44/A */
+
+#include<bits/stdc++.h>
+using namespace std;    
+int main()
+{
+    int n;
+    cin >> n;
+    vector<pair<string,string>> v(n);
+ 
+    for ( int i = 0; i < n; i++ ) 
+    {
+        cin >> v[i].first >> v[i].second;
+    }
+    sort ( v.begin(), v.end() );
+    
+    int Sz = unique ( v.begin(), v.end() ) - v.begin();
+    cout << Sz << endl;
+}
